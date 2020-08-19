8df50149857ab14343422da9183f5555c781adbe
raffayhusayn <raffayhusayn@gmail.com>
2020-08-19 05:17:11 +0500

    Contains a function get_recommendations_with_id() which takes tmdb ids as input and returns tmdb ids as outputs

---
+++ Cosine-Similarity.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import numpy as np
+from ast import literal_eval
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+from sklearn.feature_extraction.text import  CountVectorizer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+import warnings; warnings.simplefilter('ignore')
+
+md = pd.read_csv('movies_metadata.csv')
+links_small = pd.read_csv('links_small.csv')
+credits = pd.read_csv('credits.csv')
+keywords = pd.read_csv('keywords.csv')
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    return titles.iloc[movie_indices]
+
+def get_recommendations_with_id(id):
+    idx = indices_id[id]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    return ids.iloc[movie_indices]
+
+
+links_small = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
+
+keywords['id'] = keywords['id'].astype('int')
+credits['id'] = credits['id'].astype('int')
+
+md['genres'] = md['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+md = md.drop([19730, 29503, 35587])
+md['id'] = md['id'].astype('int')
+
+md = md.merge(credits, on='id')
+md = md.merge(keywords, on='id')
+
+smd = md[md['id'].isin(links_small)]
+
+smd['cast'] = smd['cast'].apply(literal_eval)
+smd['crew'] = smd['crew'].apply(literal_eval)
+smd['keywords'] = smd['keywords'].apply(literal_eval)
+smd['cast_size'] = smd['cast'].apply(lambda x: len(x))
+smd['crew_size'] = smd['crew'].apply(lambda x: len(x))
+
+def get_director(x):
+    for i in x:
+        if i['job'] == 'Director':
+            return i['name']
+    return np.nan
+
+smd['director'] = smd['crew'].apply(get_director)
+
+smd['cast'] = smd['cast'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+smd['cast'] = smd['cast'].apply(lambda x: x[:3] if len(x) >=3 else x)
+
+smd['keywords'] = smd['keywords'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+
+smd['cast'] = smd['cast'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+
+smd['director'] = smd['director'].astype('str').apply(lambda x: str.lower(x.replace(" ", "")))
+smd['director'] = smd['director'].apply(lambda x: [x,x, x])
+
+s = smd.apply(lambda x: pd.Series(x['keywords']),axis=1).stack().reset_index(level=1, drop=True)
+s.name = 'keyword'
+
+s = s.value_counts()
+s[:5]
+
+s = s[s > 1]
+
+stemmer = SnowballStemmer('english')
+
+def filter_keywords(x):
+    words = []
+    for i in x:
+        if i in s:
+            words.append(i)
+    return words
+
+smd['keywords'] = smd['keywords'].apply(filter_keywords)
+smd['keywords'] = smd['keywords'].apply(lambda x: [stemmer.stem(i) for i in x])
+smd['keywords'] = smd['keywords'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+
+smd['soup'] = smd['keywords'] + smd['cast'] + smd['director'] + smd['genres']
+smd['soup'] = smd['soup'].apply(lambda x: ' '.join(x))
+
+count = CountVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+count_matrix = count.fit_transform(smd['soup'])
+
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+smd = smd.reset_index()
+titles = smd['title']
+indices = pd.Series(smd.index, index=smd['title'])
+
+indices_id = pd.Series(smd.index, index=smd['id'])
+ids = smd['id']
+
+print(get_recommendations_with_id(133694).head(10))
