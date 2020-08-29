ace2ff552ac71faaa62154642f37188422648ee5
raffayhusayn <raffayhusayn@gmail.com>
2020-08-29 22:23:29 +0500

    Added functionality to show recommendations for new movies that the model is not trained on, using Tmdb API

---
+++ Load-Model.py
@@ -0,0 +1,68 @@
+import pickle
+import pandas as pd
+import requests
+
+with open ("cosine_similarity_model", "rb") as f:
+    cosine_sim= pickle.load(f)
+
+with open ("smd_dataset", "rb") as f:
+    smd= pickle.load(f)
+
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    return titles.iloc[movie_indices]
+
+def get_recommendations_with_id(id):
+    idx = indices_id[id]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    return ids.iloc[movie_indices]
+
+titles = smd['title']
+indices = pd.Series(smd.index, index=smd['title'])
+
+ids = smd['id']
+indices_id = pd.Series(smd.index, index=smd['id'])
+
+
+indices_id_list = indices_id.tolist()
+
+
+
+#if model not trained on data, use Tmdb API to show results
+
+movie_id= 374720
+api_key = "0cc56911115acd5222ff0c526ef328f3"
+language = "en-US"
+page = 1
+if movie_id  not in indices_id_list:
+    print("not present, run tmdb api")
+    URL = f'https://api.themoviedb.org/3/movie/{movie_id}/similar'
+
+    PARAMS = {'api_key': api_key, 'language': language, 'page': page}
+
+    # sending get request and saving the response as response object
+    r = requests.get(url = URL, params = PARAMS)
+
+    data =r.json()
+    movie_list = []
+    for movie in data['results']:
+        movie_list.append(int(movie['id']))
+
+    api_rec = movie_list[:20]
+    print(api_rec)
+
+else:
+    print("present, run normal api")
+    model_rec= get_recommendations_with_id(movie_id).head(20)
+    model_rec_list = model_rec.tolist()
+    print(model_rec_list)
+
+#print(type(get_recommendations("Good Will Hunting").head(20)))
